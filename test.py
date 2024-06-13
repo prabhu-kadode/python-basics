8e2a6055949551ebae22bfac06a95d9b08750270
prabhu <prabhukadode1@gmail.com>
2024-06-13 18:06:05 +0530

    test changes

--- test.py
+++ test.py
@@ -1,9 +1,7 @@
-def printMessage(message):
-    print(message)
-
-printMessage("Hi hello")
-
-def addTwoNumbers(a,b):
-    print(a+b)
-
-addTwoNumbers(10,20)
+class A:
+    def __init__(self) -> None:
+        pass
+    def a(self,value):
+        print(value)
+aobj = A()
+aobj.a('hi')
